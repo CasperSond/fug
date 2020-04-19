f18f4ef176b12f7805f0092f3bf238d7ef2c0999
Casper Söndergaard <casper.sondergaard@geshdo.com>
2020-04-19 18:10:31 +0200

    update to 2

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get("/", (req, res) => {
+  res.send("Hello world - 2");
 });
 
-app.listen(port, () => console.log('Listening at port 3000')); 
-
-
-
+app.listen(port, () => console.log("Listening at port 3000"));
